aee0509401aea40bde8de2df51e2f55126ee3237
DannieSheng <hudanyun.sheng@gmail.com>
2023-01-15 10:26:54 -0600

    Update exercises.sql

--- DataScience/SQL/TheUltimateMySQL/scripts/exercises.sql
+++ DataScience/SQL/TheUltimateMySQL/scripts/exercises.sql
@@ -31,4 +31,47 @@ CREATE TABLE employees
         middle_name VARCHAR(100),
         age INT NOT NULL,
         current_status VARCHAR(100) NOT NULL DEFAULT 'employed'
-    )
+    );
+INSERT INTO employees (last_name, first_name, age) VALUES ('chickenman', 'thomas', 87);
+
+
+
+CREATE TABLE cats
+(
+	cat_id INT AUTO_INCREMENT,
+    name VARCHAR(100),
+    breed VARCHAR(100),
+    age INT,
+    PRIMARY KEY (cat_id)
+);
+INSERT INTO cats(name, breed, age)
+VALUES  ('Ringo', 'Tabby', 4),
+       ('Cindy', 'Maine Coon', 10),
+       ('Dumbledore', 'Maine Coon', 11),
+       ('Egg', 'Persian', 4),
+       ('Misty', 'Tabby', 13),
+       ('George Michael', 'Ragdoll', 9),
+       ('Jackson', 'Sphynx', 7);
+
+-- rapid file exercise
+SELECT name, breed FROM cats;
+SELECT name, age FROM cats WHERE breed='Tabby';
+SELECT cat_id, age FROM cats WHERE cat_id=age;
+
+SELECT * FROM cats;
+SET SQL_SAFE_UPDATES = 0;
+UPDATE cats SET age=14 WHERE name='Misty';
+UPDATE cats SET breed='Shorthair' WHERE breed='Tabby';
+
+UPDATE cats SET name='Jack' WHERE name='Jackson';
+UPDATE cats SET breed='British Shorthair' WHERE name='Ringo';
+UPDATE cats SET age=12 WHERE breed='Maine Coon';
+
+DELETE FROM cats WHERE name='Egg';
+DELETE FROM cats WHERE age=4;
+
+SELECT * FROM cats WHERE age=cat_id;
+DELETE FROM cats WHERE age=cat_id;
+
+DELETE FROM cats;
+DESC cats;
