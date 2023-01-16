f4d7014e991d29dbee0791452c8261431297fb45
DannieSheng <hudanyun.sheng@gmail.com>
2023-01-16 14:21:19 -0600

    Update exercise_refining_selections.sql

--- DataScience/SQL/TheUltimateMySQL/scripts/exercise_refining_selections.sql
+++ DataScience/SQL/TheUltimateMySQL/scripts/exercise_refining_selections.sql
@@ -3,3 +3,39 @@ INSERT INTO books
     VALUES ('10% Happier', 'Dan', 'Harris', 2014, 29, 256), 
            ('fake_book', 'Freida', 'Harris', 2001, 287, 428),
            ('Lincoln In The Bardo', 'George', 'Saunders', 2017, 1000, 367);
+           
+SELECT * FROM books;
+
+SELECT 
+    title
+FROM
+    books
+WHERE
+    title LIKE '%stories%';
+
+SELECT title, pages
+FROM books
+ORDER BY pages DESC
+LIMIT 1;
+
+SELECT CONCAT(title, ' - ', released_year) AS summary
+FROM books
+ORDER BY released_year DESC
+LIMIT 3;
+
+SELECT title, author_lname
+FROM books
+WHERE author_lname LIKE '% %';
+
+SELECT title, released_year, stock_quantity
+FROM books
+ORDER BY stock_quantity, released_year DESC
+LIMIT 3;
+
+SELECT title, author_lname
+FROM books
+ORDER BY author_lname, title;
+
+SELECT UPPER(CONCAT('MY FAVORITE AUTHOR IS ', author_fname, ' ', author_lname, '!')) AS yell
+FROM books
+ORDER BY author_lname;
